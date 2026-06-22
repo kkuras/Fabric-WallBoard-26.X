d6afef6410c38e4b235a459733daa3f06e8e235f
kkura <flkuratomijr@gmail.com>
2026-06-22 01:00:02 -0300

    iniciando meu mod

---
+++ src/main/java/net/kkura/wallboard/WallBoard.java
@@ -0,0 +1,18 @@
+package net.kkura.wallboard;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WallBoard implements ModInitializer {
+	public static final String MOD_ID = "wallboard";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+
+}
---
+++ src/main/java/net/kkura/wallboard/WallBoardDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kkura.wallboard;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WallBoardDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
