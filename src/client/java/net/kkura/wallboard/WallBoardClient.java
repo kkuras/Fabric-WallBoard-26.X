ca2d64b1b020cbdf82b1a6e8c7462baefef64fd1
flavio <rm571211@fiap.com.br>
2026-08-14 09:36:58 -0300

    inicializador do client

---
+++ src/client/java/net/kkura/wallboard/WallBoardClient.java
@@ -0,0 +1,10 @@
+package net.kkura.wallboard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WallBoardClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
